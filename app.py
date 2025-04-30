9400a89ee245f8658271180dba92feeada2e0a71
SULINGMO <jiaruikang1228@gmail.com>
2025-04-30 15:26:07 +0800

    organise backend

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask,render_template
+import os
+from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+from src.backend.models.compare_model import CompanyGroup
+from src.backend.db import db
+
+# Load environment variables
+
+load_dotenv()
+
+# Create an app factory function
+def create_app():
+    app = Flask(__name__, template_folder='src/public')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+
+    from src.backend.models.compare_model import categorize_token, weights
+    from src.backend.routes.compare_route import compare_bp
+    app.register_blueprint(compare_bp)
+
+    @app.route('/compare')
+    def compare():
+        return render_template('compare_name/compare_name.html')
+
+    return app
+
+
+   
+
+if __name__ == '__main__':
+    app = create_app()  # ✅ Use the properly configured app
+    print(app.url_map)  # Optional: confirm all routes
+    app.run(debug=True)
+

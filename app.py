ce8afb859f39d8efa33a867b593952f67906b9ff
SULINGMO <jiaruikang1228@gmail.com>
2025-05-06 17:20:45 +0800

    update app

--- app.py
+++ app.py
@@ -21,9 +21,7 @@ def create_app():
     from src.backend.routes.match_route import match_bp 
     app.register_blueprint(compare_bp)
     app.register_blueprint(match_bp)
-    @app.route('')
-    def home():
-        return render_template('index.html')
+
     @app.route('/compare')
     def compare():
         return render_template('compare_name.html')
